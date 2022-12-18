ac3c1a31452937589e827765a09e8b746d63703e
Pavel Khudenko <khudenk0pave1@gmail.com>
2022-12-18 16:38:39 +0300

    Second commit_2

--- src/oneTwo.java
+++ src/oneTwo.java
@@ -1,5 +1,11 @@
 public class oneTwo {
     public static void main(String[] args) {
-
+    int[] num = new int[5];
+        num[0] = 1111;
+        num[1] = 14;
+        num[2] = 87;
+        num[3] = 33;
+        num[4] = 100;
+        System.out.println(num[3]);
     }
 }
